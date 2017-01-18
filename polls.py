21063b2d7faaef0627be600a0364a8ddb112ab8e
cyrilmanuel <cyriljeanneret@gmail.com>
2017-01-18 15:10:21 +0100

    lien vers les google docs et ajout de templates

--- polls.py
+++ polls.py
@@ -1,6 +1,36 @@
-from flask import Flask
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def poll_list():
+    worksheets_list = spreadsheet.worksheets()
+    return render_template('home.html', polls=worksheets_list)
+    
+@app.route('/<poll_slug>', methods=['GET'])
+def display_poll(poll_slug):
+    try:
+        poll = spreadsheet.worksheet(poll_slug)
+    except gspread.exceptions.WorksheetNotFound:
+        return "Sorry, no poll at that url", 404
+    
+    options = poll.row_values(1)
+    title = options.pop(0)
+    return render_template('poll.html', **locals())
+
+@app.route('/<poll_slug>', methods=['POST'])
+def vote(poll_slug):
+    try:
+        poll = spreadsheet.worksheet(poll_slug)
+    except gspread.exceptions.WorksheetNotFound:
+        return "Sorry, no poll at that url", 404
+
+    voter = request.form['who']
+    poll.insert_row([voter], index=2)
+    return redirect(url_for('display_poll', poll_slug=poll_slug))
+
+scope = ['https://spreadsheets.google.com/feeds']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('cred.json', scope)
+gc = gspread.authorize(credentials)
+spreadsheet = gc.open('Polls')
